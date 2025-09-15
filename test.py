e49b96b105bac1a15cf1be1fc3a1616f446dfe97
Premall <premal.choudhary@digiprima.co>
2025-09-15 16:26:13 +0530

    changes

---
+++ test.py
@@ -0,0 +1,6 @@
+# # test.py
+# import whisper
+
+# model = whisper.load_model("base")  # or "small", "medium", "large"
+# result = model.transcribe("Gulabi Saawariya Ek Chatur Naar 320 Kbps.mp3")
+# print(result["text"])
